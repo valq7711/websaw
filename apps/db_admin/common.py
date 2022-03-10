8e349c202b1415334ba26576b81cbfa853fcfa39
John Bannister <eudorajab1@gmail.com>
2022-03-10 14:16:19 +0000

    Added name to contect initialisation as per mixins

--- apps/db_admin/common.py
+++ apps/db_admin/common.py
@@ -18,28 +18,4 @@ class Context(DefaultContext):
     db_reg = DBRegistry()  
 
 ctx_ = Context()
-app = DefaultApp(ctx_, dict(group_name='websaw_apps_group_one'))
-
-
-
-#not tested but should work
-#ctx.db_reg.dbs_keys - all db-keys of the current app
-#I mean DAL-fixture  - should be fixed, or you can just 
-#class myDAL(DAL):
-#    def app_mounted(self, ctx):
-#        # see above
-#and use myDAL  instead of DAL everywhere
-#as variant you can self.dbs_meta = {}
-#and db_reg.dbs_meta[self_key] = some_meta_about_this_db
-
-'''
-
-
-
-class Context(DefaultContext):
-    auth = auth
-    default_db = db
-    
-ctx_ = Context()
-app = DefaultApp(ctx_, dict(group_name='websaw_apps_group_one'))
-'''
+app = DefaultApp(ctx_, dict(group_name='websaw_apps_group_one'), name=__package__ )
