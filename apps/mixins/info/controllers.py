92eef28b8fd2691c50020fecd11a2f23eeadf552
Val K <valq7711@gmail.com>
2022-04-01 00:17:23 +0300

    Fix examples apps

--- apps/mixins/info/controllers.py
+++ apps/mixins/info/controllers.py
@@ -1,9 +1,14 @@
-from websaw import DefaultApp, DefaultContext
-from websaw.fixtures import Env
+import os
+from websaw import DefaultApp, DefaultContext, Reloader
+
+pjoin = os.path.join
 
 
 class Context(DefaultContext):
-    env = {'mixin_env': 'mixin env is not mixed-in since it is not secure'}
+    env = {
+        'mixin_env': 'mixin env is not mixed-in since it is not secure',
+        'templ_dir': Reloader.package_folder_path(__package__, 'templates')
+    }
     ...
 
 
@@ -17,13 +22,33 @@ app = DefaultApp(ctx_, name=__package__)
 @app.route('welcome')
 @app.use('welcome_templ:welcome.html')
 def welcome(ctx):
-    return dict(msg='Hey! this is message from info-mixin cntroller')
+    msg = (
+        'Hey! this is message from info-mixin cntroller'
+        'It uses mixin template'
+    )
+    return dict(msg=msg)
 
 
 @app.route('welcome_template_overwritten')
 @app.use('welcome_templ_overwrite:welcome.html')
 def welcome_overwite_template(ctx):
-    return dict(msg='Hey! this is message from info-mixin cntroller')
+    msg = (
+        'Hey! this is message from info-mixin cntroller '
+        'It uses named template `welcome_templ_overwrite` '
+        'which could be overwritten by app context (like this one)'
+    )
+    return dict(msg=msg)
+
+
+@app.route('welcome_template_from_env')
+@app.use('{templ_dir}/welcome.html')
+def welcome_template_env(ctx):
+    msg = (
+        'Hey! this is message from info-mixin cntroller '
+        "It uses template `welcome.html` from ctx.env['templ_dir'] "
+    )
+    return dict(msg=msg)
+
 
 @app.route('info/app')
 def info_app(ctx: Context):
