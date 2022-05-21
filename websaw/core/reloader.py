8c68577aeda4157d26aa3202c39b18365e76c3af
Val K <valq7711@gmail.com>
2022-05-21 15:17:03 +0300

    fix reloader

--- websaw/core/reloader.py
+++ websaw/core/reloader.py
@@ -1,4 +1,3 @@
-import re
 import sys
 import os
 import click
@@ -9,7 +8,6 @@ from typing import Dict
 from .globs import app as om_app
 from .loggers import error_logger, get_error_snapshot
 from .utils import module2filename
-from .static_registry import static_registry
 
 from .core_events import core_event_bus, CoreEvents
 
@@ -28,8 +26,7 @@ class Reloader:
 
     modules = {}
     errors = {}
-    apps_data: Dict[str, list] = {}
-    current_import_app: str
+    registered_apps: Dict[str, object] = {}
     forget_package = staticmethod(_clear_modules)
 
     @staticmethod
@@ -46,6 +43,10 @@ class Reloader:
         return os.environ["WEBSAW_APPS_FOLDER"]
 
     @classmethod
+    def get_apps_folder_name(cls):
+        return os.path.split(cls.get_apps_folder())[-1]
+
+    @classmethod
     def package_folder(cls, package_name):
         pdir = os.path.dirname(sys.modules[package_name].__file__)
         return pdir
@@ -55,63 +56,32 @@ class Reloader:
         return os.path.join(cls.package_folder(package_name), *parts)
 
     @classmethod
-    def register_app_data(cls, app_data):
-        app_lst = cls.apps_data.setdefault(cls.current_import_app, [])
-        app_lst.append(app_data)
+    def register_app(cls, app_name, app):
+        cls.registered_apps[app_name] = app
 
     @classmethod
     def install_reloader_hook(cls):
         core_event_bus.on(CoreEvents.RELOAD_APPS, cls.reimport_apps)
 
     @classmethod
-    def expand_apps_to_reload(cls, *app_names):
-        ret = set()
-        for app_name in app_names:
-            ret.add(app_name)
-            module_name = f"apps.{app_name}"
-            app = getattr(sys.modules.get(module_name), 'app', None)
-            get_all_clients = getattr(app, 'get_all_clients', None)
-            if not get_all_clients:
-                continue
-            clients = get_all_clients()
-            for m_name in (capp._module_name for capp in clients):
-                if not re.match(r'apps\.\w+', m_name):
-                    continue
-                ret.add(m_name.split('.')[1])
-        return ret
-
-    @classmethod
-    def clear_routes(cls, app_name=''):
-        app_name.strip('/')
-        root = f'/{app_name}' if app_name else ''
-        routes = f'{root}/*'
-        om_app.router.remove(routes)
-        if root:
-            om_app.router.remove(root)
-            root_slash = f'{root}/'
-            for app_data in cls.apps_data.pop(app_name, []):
-                for route in app_data.routes:
-                    if route.rule == root or route.rule.startswith(root_slash):
-                        # already removed
-                        continue
-                    om_app.router.remove(route)
-                static_registry.unregister(app_data.static_base_url, app_name)
-
-    @classmethod
-    def reimport_apps(cls, *apps):
-        if not apps:
-            return
-        apps = cls.expand_apps_to_reload(*apps)
-        for app in apps:
-            cls.forget_package(f"apps.{app}")
-            cls.clear_routes(app)
-        for app in apps:
-            cls.import_app(app, clear_before_import=False)
+    def reimport_apps(cls, *app_names):
+        reload_all = not app_names or set(app_names) - set(cls.registered_apps)
+        apps_folder_name = cls.get_apps_folder_name()
+        if reload_all:
+            pack_names = os.listdir(cls.get_apps_folder())
+        else:
+            pack_names = app_names
+        for pack_name in pack_names:
+            app = cls.registered_apps.pop(pack_name, None)
+            if app is not None:
+                app.unmount()
+            cls.forget_package(f"{apps_folder_name}.{pack_name}")
+        for pack_name in pack_names:
+            cls.import_apps_package(pack_name)
 
     @classmethod
     def import_apps(cls):
         """Import or reimport modules"""
-        cls.clear_routes()
         folder = cls.get_apps_folder()
         # if first time reload dummy top module
         if not cls.modules:
@@ -119,68 +89,63 @@ class Reloader:
             loader = importlib.machinery.SourceFileLoader("apps", path)
             loader.load_module()
         # Then load all the apps as submodules
-        for app_name in os.listdir(folder):
-            cls.import_app(app_name, clear_before_import=False)
+        for pack_name in os.listdir(folder):
+            cls.import_apps_package(pack_name)
 
     @classmethod
-    def import_app(cls, app_name: str, clear_before_import=True):
+    def import_apps_package(cls, pack_name: str):
         folder = cls.get_apps_folder()
-        path = os.path.join(folder, app_name)
+        path = os.path.join(folder, pack_name)
         init = os.path.join(path, "__init__.py")
 
         if not (
-            not app_name.startswith("__")
+            not pack_name.startswith("__")
             and os.path.isdir(path)
             and os.path.exists(init)
         ):
             return
 
-        if clear_before_import:
-            cls.clear_routes(app_name)
-        cls.current_import_app = app_name
-
-        module_name = f"apps.{app_name}"
-        is_loaded = module_name in sys.modules
+        apps_folder_name = cls.get_apps_folder_name()
+        full_pack_name = f"{apps_folder_name}.{pack_name}"
+        is_loaded = full_pack_name in sys.modules
         try:
-            module = cls.modules.get(app_name)
+            module = cls.modules.get(pack_name)
             if module is None:
                 if is_loaded:
-                    click.secho(f"[X] already loaded {app_name}       ", fg="green")
+                    click.secho(f"[X] already loaded {pack_name}       ", fg="green")
                 else:
-                    click.echo(f"[ ] loading {app_name} ...")
+                    click.echo(f"[ ] loading {pack_name} ...")
             else:
                 if is_loaded:
-                    click.secho(f"[X] already reloaded {app_name}       ", fg="green")
+                    click.secho(f"[X] already reloaded {pack_name}       ", fg="green")
                 else:
-                    click.echo(f"[ ] reloading {app_name} ...")
+                    click.echo(f"[ ] reloading {pack_name} ...")
                     # forget the module
-                    del cls.modules[app_name]
+                    del cls.modules[pack_name]
 
             if not is_loaded:
-                module = importlib.machinery.SourceFileLoader(
-                    module_name, init
-                ).load_module()
+                module = importlib.machinery.SourceFileLoader(full_pack_name, init).load_module()
                 if hasattr(module, 'websaw_main'):
                     module.websaw_main()
-                click.secho(f"\x1b[A[X] loaded {app_name}       ", fg="green")
-            cls.modules[app_name] = module
-            cls.errors[app_name] = None
+                click.secho(f"\x1b[A[X] loaded {pack_name}       ", fg="green")
+            cls.modules[pack_name] = module
+            cls.errors[pack_name] = None
         except Exception as err:
-            cls.errors[app_name] = traceback.format_exc()
+            cls.errors[pack_name] = traceback.format_exc()
             error_logger.log(
-                app_name, get_error_snapshot()
+                pack_name, get_error_snapshot()
             )
             click.secho(
-                f"\x1b[A[FAILED] loading {app_name} ({err})",
+                f"\x1b[A[FAILED] loading {pack_name} ({err})",
                 fg="red",
             )
             # clear all files/submodules if the loading fails
-            cls.forget_package(module_name)
-            return None
+            cls.forget_package(full_pack_name)
 
-    @staticmethod
-    def get_registered_routes():
+    @classmethod
+    def get_registered_routes(cls):
         routes = []
+        apps_folder = cls.get_apps_folder()
         for route in om_app.routes.values():
             for method, method_obj in route.methods.items():
                 func = method_obj.handler
@@ -189,7 +154,7 @@ class Reloader:
                     {
                         "rule": rule,
                         "method": method,
-                        "filename": module2filename(func.__module__),
+                        "filename": module2filename(func.__module__, apps_folder),
                         "action": func.__name__,
                     }
                 )
