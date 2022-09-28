84f8f1dfc3f1a05ea6b7391761e2f5d9b6a54cda
John Bannister <eudorajab1@gmail.com>
2022-09-28 19:13:47 +0200

    overhaul on scaffold app added db_admin mixin

--- apps/scaffold/controllers.py
+++ apps/scaffold/controllers.py
@@ -4,8 +4,10 @@ from websaw.fixtures import Env, XAuth
 from websaw.templates import UTemplate
 import ombott
 
-from ..mixins import info, auth
+from ..mixins import auth, db_admin
+
 from . import utemplates as ut
+from .app_db import db
 
 ombott.default_app().setup(dict(debug=True))
 
@@ -17,24 +19,14 @@ class GetUserMenus(XAuth):
         user = ctx.current_user.user
         ctx.env['default_template_context']['menu'] = [
             {'label': 'Home', 'href':ctx.URL('index')},
-            {'label': 'About', 'href':ctx.URL('about')}
+            {'label': 'About', 'href':ctx.URL('about')},
+            {'label': 'DB Admin', 'href':ctx.URL('db_admin')}
             ]
 
 menu=GetUserMenus()
 
-
-# make a simple custom fixture that uses another fixture (session)
-class LastVisited(Fixture):
-    def take_off(self, ctx: DefaultContext):
-        session = ctx.session  # magic goes here - we touch session and ctx activates it!
-        last_visited = session.get('last_visited', [])
-        last_visited.append(ctx.request.path)
-        last_visited = last_visited[-5:]
-        session['last_visited'] = last_visited
-
-# make a simple fixture that deals with the navbar. This can be extended to use auth
 # extend default context with our fixture and info-mixin context
-class Context(info.Context, auth.Context, DefaultContext):
+class Context(db_admin.Context, auth.Context, DefaultContext):
     env={
         'menus' : [],
         'menu': [],
@@ -42,34 +34,30 @@ class Context(info.Context, auth.Context, DefaultContext):
         'default_template_context': dict(buttons = ''),
         
     }
-    track_visited = LastVisited()
-    index_template = UTemplate(ut.index)
+    db = db
     menu=menu
 
 ctxd = Context()
 app = DefaultApp(ctxd, config=dict(group_name='websaw_apps_group_one'), name=__package__)
 
 # use mixin(s)
-app.mixin(info.app, auth.app)
+app.mixin(db_admin.app, auth.app)
 
 @app.route('index')
-@app.use(ctxd.menu, ctxd.track_visited, ctxd.index_template)  # note there is no session, but it used!
+@app.use(ctxd.menu, ut.index)  # note there is no session, but it used!
 def index(ctx: Context):
     user=ctx.current_user.user
-    
-    print('User is ', user)
-    ret = {
-        k: ctx[k]
-        for k in 'app_name base_url static_base_url folder template_folder static_folder'.split()
-    }
-    ret['app_data_keys'] = [*ctx.app_data.__dict__]
-    ret['env'] = ctx.env
-    ret['ctx_fixtures'] = {k: repr(f) for k, f in ctx._fixt.items()}
+    session = ctx.session
+    flash = ctx.flash
+    ret = 'You can customise this page by adding any components or functionality you need'
     return dict(msg=ret)
 
 @app.route('about')
-@app.use(ctxd.menu, ctxd.track_visited, ctxd.index_template)  # note there is no session, but it used!
-def index(ctx: Context):
+@app.use(ctxd.menu, ut.about)  # note there is no session, but it used!
+def about(ctx: Context):
+    user=ctx.current_user.user
+    flash = ctx.flash
+    
     ret = {
         k: ctx[k]
         for k in 'app_name base_url static_base_url folder template_folder static_folder'.split()
@@ -78,50 +66,4 @@ def index(ctx: Context):
     ret['env'] = ctx.env
     ret['ctx_fixtures'] = {k: repr(f) for k, f in ctx._fixt.items()}
     return dict(msg=ret)
-
-
-@app.route('set_env')
-@app.use(Env(foo='change foo value', bar='bar value'))
-def set_env(ctx: Context):
-    return ctx.env
-
-
-# Use the  local 'private' index template
-@app.route('session')
-@app.use(ut.index)
-def session(ctx: Context):
-    ctx.env['default_template_context']['user'] = 'Fred Elliot'
-    ret = {
-        'group_session_data': {**ctx.group_session},
-        'session_data': {**ctx.session},
-        'local_data_keys': [*ctx.session.data.__dict__],
-
-    }
-    return dict(msg=ret)
-
-
-# use mixin template
-@app.route('use_mixin_template')
-@app.use(ctxd.welcome_template)
-def app_welcome(ctx: Context):
-    return dict(msg='Hey! This is message from app controller using the INFO mixin template')
-
-# use another specific template
-@app.route('upytl_demo')
-@app.use(ut.upytl_demo)
-def upytl_demo(ctx: Context):
-    return dict(msg='Hey! This page is rendered using UPYTL and the upytl_demo Template')
-
-
-# let's go to:
-# http://127.0.0.1:8000/scaffold
-# http://127.0.0.1:8000/scaffold/index
-# http://127.0.0.1:8000/scaffold/session
-# http://127.0.0.1:8000/scaffold/use_mixin_template
-# http://127.0.0.1:8000/scaffold/upytl_demo
-
-# provided by mixins/info
-# http://127.0.0.1:8000/scaffold/welcome
-# http://127.0.0.1:8000/scaffold/mixin_template_overwritten
-# http://127.0.0.1:8000/simple/info/app
 
--- apps/scaffold/utemplates.py
+++ apps/scaffold/utemplates.py
@@ -1,7 +1,7 @@
 from upytl import SlotTemplate, html as h 
 
 from upytl_standard import HTMLPage, StandardNavBar 
-
+from ..common.common_components import Flash
 # flake8: noqa E226
 
 index = {
@@ -9,35 +9,42 @@ index = {
         SlotTemplate(Slot='nav'):{
             StandardNavBar(menu={'menu'}, user= {'user'}, buttons={'buttons'}): '',
         },
-        SlotTemplate(Slot='flash'):{},
+        SlotTemplate(Slot='flash'):{
+            Flash(flash=()):{}
+        },
         
         SlotTemplate(Slot='content'):{
-            h.Div(Class='box'):{
-                h.Div(Class='title is-4'): 'Welcome [[user]] from the default_template_context',
-                h.Div(Class='title is-5'): 'This is the Scaffold App Template',
-                h.Div(For='f in msg'):{
-                    h.Text():'[[ f ]] : [[msg[f] ]]',
+            h.Div(Class='section'):{
+                h.Div(Class='box'):{
+                    h.Div(Class='title is-4'): 'Welcome [[user]] to the scaffold app',
+                    h.Div(Class='title is-5'): 'This is your quick start for creating beautifull apps',
+                    h.Div(Class='title has-text-centered is-default'):'[[ msg ]]',
                 }
-            }    
+            }
         }
     }
 }
 
-upytl_demo = {
-    h.Html():{
-        h.Head():{
-            h.Title():"[[app_get('app_name')]]",
-                h.Meta(charset='utf-8'):'',
-                h.Link(rel='stylesheet', href='https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.1/css/bulma.min.css'):None, 
-            },
-            h.Body():{
+about = {
+    HTMLPage(footer_class='custom-footer'):{
+        SlotTemplate(Slot='nav'):{
+            StandardNavBar(menu={'menu'}, user= {'user'}, buttons={'buttons'}): '',
+        },
+        SlotTemplate(Slot='flash'):{
+            Flash(flash=()):{}
+        },
+        
+        SlotTemplate(Slot='content'):{
+            h.Div(Class='section'):{
                 h.Div(Class='box'):{
-                    h.Div(Class='title'):'[[msg]]',
+                    h.Div(Class='title is-5'): 'This is all the information related to the scaffold app. You can format all in the about template',
+                    h.Div(For='f in msg'):{
+                        h.Text():'[[ f ]] : [[msg[f] ]]',
+                    }
                 }
-            },    
-            h.Footer():{
-                h.Div(): 'This is the footer',
-            }
-
+            }    
         }
+        
     }
+}
+
